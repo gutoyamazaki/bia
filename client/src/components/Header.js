914cd9456253824b38e7c1ca9956d16ff71128c1
Augusto Yamazaki <guto_yamazaki@hotmail.com>
2025-02-07 11:06:06 -0300

    alterando header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,7 +11,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {location.pathname === "/" && (
         <Button
           color={showAdd ? "red" : "blue"}
-          text={showAdd ? "Close" : "Adicionar"}
+          text={showAdd ? "Fechar" : "Adicionar"}
           onClick={onAdd}
         />
       )}
